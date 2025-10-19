41f1560b73995522eb6e5f1e416d53850d4991bd
andreea1306 <andreealdea13@gmail.com>
2025-10-19 13:06:25 +0300

    Aufgabe2 - functii calculare suma

--- src/ArrayOperations.java
+++ src/ArrayOperations.java
@@ -20,4 +20,23 @@ public class ArrayOperations {
         return min;
     }
 
+    public int calculateTotalSum(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+
+    public int calculateMaxSum(int[] numbers) {
+        int totalSum = calculateTotalSum(numbers);
+        int min = findMin(numbers);
+        return totalSum - min;
+    }
+
+    public int calculateMinSum(int[] numbers) {
+        int totalSum = calculateTotalSum(numbers);
+        int max = findMax(numbers);
+        return totalSum - max;
+    }
 }
