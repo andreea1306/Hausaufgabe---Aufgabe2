9bf181609cf87ff965b2e59bbacf1e514498eeb3
andreea1306 <andreealdea13@gmail.com>
2025-10-19 12:59:05 +0300

    Aufgabe2 - functie gasire minim si maxim

---
+++ src/ArrayOperations.java
@@ -0,0 +1,23 @@
+public class ArrayOperations {
+
+    public int findMax(int[] numbers) {
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
+
+    public int findMin(int[] numbers) {
+        int min = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        return min;
+    }
+
+}
